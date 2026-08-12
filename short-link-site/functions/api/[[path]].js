76775f66ee8af4d8df0c94290096e401ab6f1715
lovetoyuno <hero@china.megumi.qzz.io>
2026-08-12 19:32:05 +0800

    Delete short-link-site directory

--- short-link-site/functions/api/[[path]].js
+++
@@ -1,8 +0,0 @@
-import { handleApiRequest } from "../_shared.js"
-
-export async function onRequest(context) {
-	const { request, env, params } = context
-	const url = new URL(request.url)
-	const pathParts = Array.isArray(params.path) ? params.path : params.path ? [params.path] : []
-	return handleApiRequest(pathParts, request, env, url)
-}
